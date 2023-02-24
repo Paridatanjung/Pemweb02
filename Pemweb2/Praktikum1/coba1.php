f301190ba50fac95eee0512915bafe62020dca6d
Paridatanjung <114472326+Paridatanjung@users.noreply.github.com>
2023-02-24 16:04:03 +0700

    Tugas dan Praktikum 1 PHP

---
+++ Pemweb2/Praktikum1/coba1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+define( 'PHI',3.14)
+define( 'DBNAME', 'inventori');
+define( 'DBSERVER', 'localhost');
+
+echo 'Nama databes nya : '  . DBNAME;
+echo '<br> Lokasi databes nya : '  . DBSERVER;
---
+++ Pemweb2/Praktikum1/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nama = "Parida";
+$umur = 17;
+$berat = 45;
+
+
+echo "Nama saya : "  .  $nama;
+echo "<br> Umur saya : "  . $umur;
+echo "<br> Berat saya : "  . $berat;
+
