365ee770868ba2b32f8c796fd9041cc6bfb8f530
Paridatanjung <114472326+Paridatanjung@users.noreply.github.com>
2023-03-10 15:36:53 +0700

    Praktikum dan Tugas 3 PHP

---
+++ Praktikum3/footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <p>Create by
+            <a href=""> PARIDA  &copy;2017</a>
+        </p>
+    <footer>
+
+</body>
+</html>
---
+++ Praktikum3/score.php
@@ -0,0 +1,9 @@
+<?php
+    include_once 'header.php';
+?>
+
+<h1>Ini Halaman Score</h1>
+
+<?php
+    include_once'footer.php';
+?>
